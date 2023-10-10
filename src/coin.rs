f9f708b72826e29a2cd84b296ec103ac22604e28
Bly Kim <blykim@cluml.com>
2023-10-10 17:31:52 +0900

    Replaced const

--- src/coin.rs
+++ src/coin.rs
@@ -6,8 +6,10 @@ use termion::input::TermRead;
 use termion::raw::RawTerminal;
 
 const MANUAL_POS: u16 = 23;
-const PLAY_TIME: u64 = 10;
+const PLAY_TIME: u64 = 60;
 const COIN_TIME: u64 = 2;
+const MAIN_POINT: u16 = 2;
+const SIZE: u16 = 20;
 
 struct Position {
     x: u16,
@@ -21,19 +23,22 @@ pub fn coin_game<R: Read>(stdin: &mut R, stdout: &mut RawTerminal<StdoutLock>) {
         stdout,
         "{}{}q to exit. Use arrow keys to move the character.{}",
         termion::clear::All,
-        termion::cursor::Goto(MANUAL_POS, 2),
+        termion::cursor::Goto(MANUAL_POS, MAIN_POINT),
         termion::cursor::Hide
     )
     .unwrap();
 
-    let mut player = Position { x: 2, y: 2 };
+    let mut player = Position {
+        x: MAIN_POINT,
+        y: MAIN_POINT,
+    };
     let mut score = 0;
 
     // coin
     let mut rng = rand::thread_rng();
     let mut coin = Position {
-        x: rng.gen_range(2..20),
-        y: rng.gen_range(2..20),
+        x: rng.gen_range(MAIN_POINT..SIZE),
+        y: rng.gen_range(MAIN_POINT..SIZE),
     };
 
     draw_border(stdout);
@@ -53,8 +58,8 @@ pub fn coin_game<R: Read>(stdin: &mut R, stdout: &mut RawTerminal<StdoutLock>) {
             coin_time = time::Instant::now();
             clear_coin(stdout, &coin);
 
-            coin.x = rng.gen_range(2..20);
-            coin.y = rng.gen_range(2..20);
+            coin.x = rng.gen_range(MAIN_POINT..SIZE);
+            coin.y = rng.gen_range(MAIN_POINT..SIZE);
             draw_coin(stdout, &coin);
         }
 
@@ -62,8 +67,8 @@ pub fn coin_game<R: Read>(stdin: &mut R, stdout: &mut RawTerminal<StdoutLock>) {
             score += 1;
             coin_time = time::Instant::now();
 
-            coin.x = rng.gen_range(2..20);
-            coin.y = rng.gen_range(2..20);
+            coin.x = rng.gen_range(MAIN_POINT..SIZE);
+            coin.y = rng.gen_range(MAIN_POINT..SIZE);
             draw_coin(stdout, &coin);
         } else {
             clear_player(stdout, &player);
@@ -72,22 +77,22 @@ pub fn coin_game<R: Read>(stdin: &mut R, stdout: &mut RawTerminal<StdoutLock>) {
         match c.unwrap() {
             Key::Char('q') => break,
             Key::Left => {
-                if player.x > 2 {
+                if player.x > MAIN_POINT {
                     player.x -= 1;
                 }
             }
             Key::Right => {
-                if player.x < 20 {
+                if player.x < SIZE {
                     player.x += 1;
                 }
             }
             Key::Up => {
-                if player.y > 2 {
+                if player.y > MAIN_POINT {
                     player.y -= 1;
                 }
             }
             Key::Down => {
-                if player.y < 20 {
+                if player.y < SIZE {
                     player.y += 1;
                 }
             }
@@ -98,8 +103,8 @@ pub fn coin_game<R: Read>(stdin: &mut R, stdout: &mut RawTerminal<StdoutLock>) {
             score += 1;
             coin_time = time::Instant::now();
 
-            coin.x = rng.gen_range(2..20);
-            coin.y = rng.gen_range(2..20);
+            coin.x = rng.gen_range(MAIN_POINT..SIZE);
+            coin.y = rng.gen_range(MAIN_POINT..SIZE);
             draw_coin(stdout, &coin);
         }
 
@@ -108,7 +113,7 @@ pub fn coin_game<R: Read>(stdin: &mut R, stdout: &mut RawTerminal<StdoutLock>) {
         write!(
             stdout,
             "{}Score: {}",
-            termion::cursor::Goto(MANUAL_POS, 5),
+            termion::cursor::Goto(MANUAL_POS, MAIN_POINT + 3),
             score
         )
         .unwrap();
@@ -127,21 +132,21 @@ pub fn coin_game<R: Read>(stdin: &mut R, stdout: &mut RawTerminal<StdoutLock>) {
 }
 
 fn draw_border(stdout: &mut RawTerminal<StdoutLock>) {
-    for i in 1..22 {
+    for i in (MAIN_POINT - 1)..(SIZE + 2) {
         write!(
             stdout,
             "{}#{}#",
-            termion::cursor::Goto(i, 1),
-            termion::cursor::Goto(i, 21)
+            termion::cursor::Goto(i, MAIN_POINT - 1),
+            termion::cursor::Goto(i, SIZE + 1)
         )
         .unwrap();
 
-        if i < 20 {
+        if i < SIZE {
             write!(
                 stdout,
                 "{}#{}#",
-                termion::cursor::Goto(1, i + 1),
-                termion::cursor::Goto(21, i + 1)
+                termion::cursor::Goto(MAIN_POINT - 1, i + 1),
+                termion::cursor::Goto(SIZE + 1, i + 1)
             )
             .unwrap();
         }
